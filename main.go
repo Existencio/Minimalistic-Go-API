ff12a2193614a9681dffc0e73d2969e2f2f9b0ca
Existencio <arthemios@my.com>
2026-04-28 00:56:13 +0300

    feat: wire posts server
    Load posts on startup, register the /posts handler, start the HTTP server on port 8080, and ignore local posts.json data.

--- main.go
+++ main.go
@@ -1,4 +1,28 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+
+	"minimalistic-go-api/handlers"
+	"minimalistic-go-api/storage"
+)
+
 func main() {
+	// ! Загружаем посты из файла при старте
+	storage.LoadPosts()
+
+	// ! Регистрируем обработчик
+	http.HandleFunc("/posts", handlers.PostHandler)
+	// ? т.е Когда придёт запрос на /posts, вызови функцию handlers.PostHandler.
+
+	fmt.Println("Server started on port 8080")
+
+	// ! Запускаем сервер
+	err := http.ListenAndServe(":8080", nil)
+	// ? второй параметр это главный обработчик запросов
+	// ? Если передать nil, Go использует стандартный роутер: http.DefaultServeMux
+	if err != nil {
+		fmt.Println("server error:", err)
+	}
 }
